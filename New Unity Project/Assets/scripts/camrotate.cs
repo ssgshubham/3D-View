d982072565bde9935dfea86751df0c427eb87842
ssgshubham <62928567+ssgshubham@users.noreply.github.com>
2020-11-05 12:27:28 +0530

    u3v

---
+++ New Unity Project/Assets/scripts/camrotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camrotate : MonoBehaviour
+{
+    public GameObject target;
+    float speed = 5;
+
+    float minFov = 35f;
+    float maxFov = 100f;
+    float senssitivity = 17f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            transform.RotateAround(target.transform.position, transform.up, Input.GetAxis("Mouse X") * speed);
+            transform.RotateAround(target.transform.position, transform.right, Input.GetAxis("Mouse Y") * -speed);
+        }
+
+        float fov = Camera.main.fieldOfView;
+        fov += Input.GetAxis("Mouse ScrollWheel") * -senssitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+    }
+}
---
+++ New Unity Project/Assets/scripts/scenedec.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenedec : MonoBehaviour
+{
+    
+    public void menue()
+   {
+       SceneManager.LoadScene(0);
+   }
+
+    public void car1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void car2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void car3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void car4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void car5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void car6()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void car7()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+
+
+    public void food1()
+    {
+        SceneManager.LoadScene(8);
+    }
+    public void food2()
+    {
+        SceneManager.LoadScene(9);
+    }
+    public void food3()
+    {
+        SceneManager.LoadScene(10);
+    }
+    public void food4()
+    {
+        SceneManager.LoadScene(11);
+    }
+    public void food5()
+    {
+        SceneManager.LoadScene(12);
+    }
+    public void food6()
+    {
+        SceneManager.LoadScene(13);
+    }
+    public void food7()
+    {
+        SceneManager.LoadScene(14);
+    }
+
+
+
+    public void shape1()
+    {
+        SceneManager.LoadScene(15);
+    }
+    public void shape2()
+    {
+        SceneManager.LoadScene(16);
+    }
+    public void shape3()
+    {
+        SceneManager.LoadScene(17);
+    }
+    public void shape4()
+    {
+        SceneManager.LoadScene(18);
+    }
+    public void shape5()
+    {
+        SceneManager.LoadScene(19);
+    }
+    public void shape6()
+    {
+        SceneManager.LoadScene(20);
+    }
+    public void shape7()
+    {
+        SceneManager.LoadScene(21);
+    }
+
+
+    public void candy1()
+    {
+        SceneManager.LoadScene(22);
+    }
+    public void candy2()
+    {
+        SceneManager.LoadScene(23);
+    }
+    public void candy3()
+    {
+        SceneManager.LoadScene(24);
+    }
+    public void candy4()
+    {
+        SceneManager.LoadScene(25);
+    }
+    public void candy5()
+    {
+        SceneManager.LoadScene(26);
+    }
+    public void candy6()
+    {
+        SceneManager.LoadScene(27);
+    }
+    public void candy7()
+    {
+        SceneManager.LoadScene(28);
+    }
+
+
+    public void Quit()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+
+}
